47487472d7a79fd65083074a880d5e6b9357d102
saleh2411 <salehkhalil1124@gmail.com>
2024-12-15 16:44:28 +0200

    main

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+//#include <ctypes.h>
+
+int boardSize;
+
+int main(){
+    printf("Please enter the board size N [1-10]:");
+    scanf("%d", &boardSize);
+    return 0;
+}
