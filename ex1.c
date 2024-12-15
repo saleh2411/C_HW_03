9ab759935941e27b381fbdd92f9145fd40f66581
wwsmms <62592223+wwsmms@users.noreply.github.com>
2024-12-15 18:56:40 +0200

    Update ex1.c
    fu

--- ex1.c
+++ ex1.c
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <string.h>
-//#include <ctypes.h>
-
-int boardSize;
-
-int main(){
-    printf("Please enter the board size N [1-10]:");
-    scanf("%d", &boardSize);
-    return 0;
-}
-=======
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
@@ -137,4 +123,3 @@ int main() {
 
     return 0;
 }
->>>>>>> origin/main
